9b747793ec15e1df9286b36a9eb4968863b1095e
vivek-344 <vivekuit344@gmail.com>
2023-08-09 15:40:46 +0530

    Update Profile

--- app/src/main/java/com/satverse/suraksha/LandingPageActivity.kt
+++ app/src/main/java/com/satverse/suraksha/LandingPageActivity.kt
@@ -5,10 +5,17 @@ import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.widget.PopupMenu
+import android.view.View
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.Toast
+import androidx.core.app.ActivityCompat.finishAffinity
 import androidx.lifecycle.lifecycleScope
 import com.satverse.suraksha.dropdown.EditProfileActivity
+import com.satverse.suraksha.dropdown.EmergencyContactsActivity
+import com.satverse.suraksha.dropdown.HowToUseActivity
 import com.satverse.suraksha.userlogin.LoginActivity
 import io.appwrite.Client
 import io.appwrite.services.Account
@@ -19,21 +26,55 @@ class LandingPageActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_landing_page)
 
-        val editProfile = findViewById<ImageView>(R.id.sosButton)
-        editProfile.setOnClickListener {
-            val intent = Intent(this, EditProfileActivity::class.java)
-            startActivity(intent)
+        val btnShowPopup: ImageView = findViewById(R.id.menu)
+
+        btnShowPopup.setOnClickListener { view ->
+            showPopupMenu(view)
+        }
+    }
+
+    private fun showPopupMenu(view: View) {
+        val popupMenu = PopupMenu(this, view)
+        popupMenu.menuInflater.inflate(R.menu.popup_menu, popupMenu.menu)
+
+        popupMenu.setOnMenuItemClickListener { item: MenuItem ->
+            when (item.itemId) {
+                R.id.emergencyContacts -> {
+
+                    val intent = Intent(this, EmergencyContactsActivity::class.java)
+                    startActivity(intent)
+
+                    true
+                }
+                R.id.howToUse -> {
+
+                    val intent = Intent(this, HowToUseActivity::class.java)
+                    startActivity(intent)
+
+                    true
+                }
+                R.id.editProfile -> {
+
+                    val intent = Intent(this, EditProfileActivity::class.java)
+                    startActivity(intent)
+
+                    true
+                }
+                R.id.logOut -> {
+
+                    lifecycleScope.launch {
+                        logOut()
+                    }
+
+                    userLoggedOut()
+
+                    true
+                }
+                else -> false
+            }
         }
 
-        val logOut = findViewById<ImageView>(R.id.emergencyButton)
-        logOut.setOnClickListener {
-            val intent = Intent(this, LoginActivity::class.java)
-            startActivity(intent)
-            lifecycleScope.launch {
-                logOut()
-            }
-            userLoggedOut()
-        }
+        popupMenu.show()
     }
 
     private suspend fun logOut() {
@@ -50,6 +91,10 @@ class LandingPageActivity : AppCompatActivity() {
 
         try {
             account.deleteSessions()
+
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+
         } catch (e: Exception) {
             runOnUiThread {
                 Toast.makeText(this, "$e", Toast.LENGTH_SHORT).show()
