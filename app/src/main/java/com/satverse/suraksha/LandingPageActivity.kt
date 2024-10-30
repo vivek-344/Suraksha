b3a392a415525b4090cf62a4d73714bc1bc56e2c
Satyam Sharma <satyamsharma1725@gmail.com>
2024-10-30 12:05:50 +0530

    LoginPageActivity.kt updated

--- app/src/main/java/com/satverse/suraksha/LandingPageActivity.kt
+++ app/src/main/java/com/satverse/suraksha/LandingPageActivity.kt
@@ -284,7 +284,7 @@ class LandingPageActivity : AppCompatActivity() {
 
         val client = Client(this)
             .setEndpoint("https://cloud.appwrite.io/v1")
-            .setProject("64bb859f2d53d0d44e9c")
+            .setProject("65fa760fe93c38713a0f")
             .setSelfSigned(true)
 
         val account = Account(client)
